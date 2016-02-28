6d8af189409d338088479ebf8ae3d869d082683c
cyang001 <cyang001@citymail.cuny.edu>
2016-02-27 22:38:32 -0500

    First commit

---
+++ Tweeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweeter
+//
+//  Created by Christopher Yang on 2/27/16.
+//  Copyright © 2016 Christopher Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
